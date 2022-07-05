4096c3c7aac7ee1c18249d11978b6c3d373935e8
Sean <sdillon215@gmail.com>
2022-07-04 23:51:11 -0700

    initial commit bruh

---
+++ android/app/src/main/kotlin/com/example/meesh_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.meesh_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:meesh_app/apps/meeesh_mobile_app/meesh_mobile_app.dart';
+
+
+void main() async {
+  runApp(const MeeshApp());
+}
